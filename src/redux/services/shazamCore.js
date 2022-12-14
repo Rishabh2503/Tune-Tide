8ca7881ae6f6c7e1dd714e74789af0668c7b9864
Rishabh2503 <gupta.rish2501@gmail.com>
2022-12-14 18:26:13 +0530

    Tune-Tide

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,19 @@
+import {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+    export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery : fetchBaseQuery({
+        baseUrl : 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders : (headers) => {
+            headers.set('X-RapidAPI-Key','0d6b40a7bemshf505c3f7573ddb5p14905fjsn1a5589314db3');
+            return headers;
+        },
+    }),
+    endpoints : (builder) => ({
+        getTopCharts : builder.query({ query : () => '/charts/world'}),
+    }),
+    });
+    export const {
+        useGetTopChartsQuery,
+
+    } = shazamCoreApi;
